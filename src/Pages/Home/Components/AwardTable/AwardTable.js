a8e0c255dfece87640754c469c66a532736f2d36
SadikHasanRafi <sadikkhasan@gmail.com>
2022-06-16 19:43:39 +0600

    education and certificate added

--- src/Pages/Home/Components/AwardTable/AwardTable.js
+++ src/Pages/Home/Components/AwardTable/AwardTable.js
@@ -1,44 +1,42 @@
-import React from 'react';
+import React from "react";
+import "./AwardTable.css"
 
 const AwardTable = () => {
-    return (
+  return (
+    <div className="m-20">
+
         <div>
-            <div class="overflow-x-auto w-full">
-            <div class="overflow-x-auto">
-  <table class="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-</div>
+            <p className="text-accent font-extrabold text-center text-5xl m-10 ">Education & Certifications</p>
         </div>
-    );
+
+      <div class="overflow-x-auto w-full ">
+        <div class="overflow-x-auto">
+          <table class="table w-full hover:shadow-lg shadow-md rounded-2xl my-8 text-center">
+            <thead >
+              <tr >
+                <th  className=" bg-primary text-accent font-extrabold text-lg normal-case" >Year</th>
+                <th className=" bg-primary text-accent font-extrabold text-lg normal-case" >Degree or Certificates</th>
+                <th className=" bg-primary text-accent font-extrabold text-lg normal-case" >Institution or University</th>
+              </tr>
+            </thead>
+            <tbody className=" text-accent">
+              <tr>
+                <td>2019 - Present</td>
+                <td>B.Sc in CSE</td>
+                <td>Daffodil International University</td>
+              </tr>
+              <tr>
+                <td >2021 - 2022</td>
+                <td>Complete web development course with Jhankar Mahbub</td>
+                <td>Programming Hero</td>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AwardTable;
